753cccaaf5fd6996a6131b55cc51f30f2c7069fd
Khaing Swe Win <khaingswewin@hostmyanmar.net>
2022-03-20 19:58:10 +0630

    added files

--- admin/index.php
+++ admin/index.php
@@ -1,30 +1,11 @@
 <?php
-session_start();
-error_reporting(0);
-include('includes/dbconnection.php');
+
 
 if(isset($_POST['login']))
   {
-    $adminuser=$_POST['username'];
-    $password=md5($_POST['password']);
-    $query=mysqli_query($con,"select ID from tbladmin where  AdminuserName='$adminuser' && Password='$password' ");
-    $ret=mysqli_fetch_array($query);
-    if($ret>0){
-      $_SESSION['adid']=$ret['ID'];
-     header('location:dashboard.php');
-    }else{
-    $msg="Invalid Details.";
-    }
+    header('location:dashboard.php');
   }
-  ?>
-
-
-
-
-
-
-
-
+?>
 <!doctype html>
 <html lang="en">
 
